327513dcd1d2acd808733ddd801dfd721179a551
ArielM24 <ariel_montoya15@outlook.es>
2021-01-13 13:54:11 -0600

    read raw code

---
+++ lib/coder.dart
@@ -0,0 +1,478 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+Future<List<_Shape>> readJson(File f) async {
+  List<Map> shapes = List<Map>();
+  String str = await f.readAsString();
+  str = '{"shapes":$str}';
+  Map<String, dynamic> data = JsonCodec().decode(str);
+  data["shapes"].forEach((e) {
+    if (e["classList"]["1"] != "line") {
+      shapes.add(e);
+    }
+  });
+
+  return getShapes(shapes);
+}
+
+class _Shape {
+  String type;
+  String id;
+  String content;
+  @override
+  String toString() => "{$type, $id, $content}";
+}
+
+class _Line extends _Shape {
+  String id1, id2;
+  @override
+  String toString() => super.toString() + " $id1, $id2";
+}
+
+List<_Shape> getShapes(List<Map> shapes) {
+  List<_Shape> l = [];
+  for (var s in shapes) {
+    String type = s["classList"]["1"];
+    if (type != "line") {
+      l.add(getShape(s));
+      print(s["id"] + ":" + s["title"]);
+    } else {
+      print("line");
+      l.add(getLine(s));
+      print("l:" + s["title"]);
+    }
+  }
+  return l;
+}
+
+_Shape getShape(Map<dynamic, dynamic> shape) {
+  _Shape s = _Shape();
+  s.type = shape["classList"]["1"];
+  s.id = shape["id"];
+  s.content = shape["title"];
+  return s;
+}
+
+_Line getLine(Map<dynamic, dynamic> shape) {
+  _Line l = _Line();
+
+  l.type = shape["classList"]["1"];
+  l.id = shape["id"];
+  l.content = shape["title"];
+  l.id1 = shape["classList"]["4"].split("id1")[1];
+  l.id2 = shape["classList"]["5"].split("id2")[1];
+
+  return l;
+}
+
+String getRaw(List<_Shape> shapes) {
+  String raw = "";
+  for (_Shape s in shapes) {
+    raw += s.content + "\n";
+  }
+  print(raw);
+  return raw;
+}
+
+class Coder {
+  int ifelse = 0; //1 -> true, 2-> else
+  List<_Shape> _shapes;
+  String raw = "";
+  Coder.build(this._shapes);
+  Coder.fromRaw(this._shapes) : raw = getRaw(_shapes);
+  String getCodeC() {
+    String code = "#include<stdio.h>\n\nint main(){\n";
+    for (_Shape s in _shapes) {
+      code += getCShape(s);
+    }
+
+    return code + "\n\treturn 0;\n}";
+  }
+
+  String getCShape(_Shape s) {
+    switch (s.type) {
+      case "rectangulo":
+        String str = '\t${s.content};\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo1":
+        String str = inC(s.content);
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo2":
+        String str = '\tprintf("${s.content}\\n");\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "rombo":
+        ifelse = 1;
+        return '\tif(${s.content}){\n\t';
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String inC(String content) {
+    var l = content.split(":");
+    String format;
+    switch (l[0]) {
+      case "int":
+        format = "%d";
+        break;
+      case "float":
+        format = "%f";
+        break;
+    }
+    return '\tscanf("$format",&${l[1]});\n';
+  }
+
+  String getCodeCpp() {
+    String code = "#include<iostream>\nusing namespace std;\nint main(){\n";
+    for (_Shape s in _shapes) {
+      code += getCppShape(s);
+    }
+
+    return code + "\n\treturn 0;\n}";
+  }
+
+  String getCppShape(_Shape s) {
+    switch (s.type) {
+      case "rectangulo":
+        String str = '\t${s.content};\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo1":
+        String str = "\tcin>>${s.content.split(':')[1]};\n";
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo2":
+        String str = '\tcout<<"${s.content}"<<endl;\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "rombo":
+        ifelse = 1;
+        return '\tif(${s.content}){\n\t';
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String getCodeJava() {
+    String code = "import java.util.Scanner;\n"
+        "public class output{\n"
+        "\tstatic Scanner scan = new Scanner(System.in);\n"
+        "\tpublic static void main(String[] args){\n";
+    for (_Shape s in _shapes) {
+      code += getJavaShape(s);
+    }
+
+    return code + "\n\t}\n}";
+  }
+
+  String getJavaShape(_Shape s) {
+    switch (s.type) {
+      case "rectangulo":
+        String str = '\t${s.content};\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo1":
+        String str = inJava(s.content);
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo2":
+        String str = '\tSystem.out.println("${s.content}");\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "rombo":
+        ifelse = 1;
+        return '\tif(${s.content}){\n\t';
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String inJava(String content) {
+    var l = content.split(":");
+    switch (l[0]) {
+      case "int":
+        return "\t${l[1]} = scan.nextInt();\n";
+        break;
+      case "float":
+        return "\t${l[1]} = scan.nextFloat();\n";
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String getCodePython() {
+    String code = "if __name__ == '__main__':\n";
+    for (_Shape s in _shapes) {
+      code += getPythonShape(s);
+    }
+
+    return code;
+  }
+
+  String getPythonShape(_Shape s) {
+    switch (s.type) {
+      case "rectangulo":
+        String str = '\t${s.content.split(" ")[1]} = 0\n';
+        if (ifelse == 1) {
+          str += "\n";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "\telse:\n\t\t" + str + "\n";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo1":
+        String str = inPython(s.content);
+        if (ifelse == 1) {
+          str += "\n";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "\telse:\n\t\t" + str + "\n";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo2":
+        String str = '\tprint("${s.content}");\n';
+        if (ifelse == 1) {
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "\telse:\n\t\t" + str + "\n";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "rombo":
+        ifelse = 1;
+        return '\tif ${s.content} :\n\t';
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String inPython(String content) {
+    var l = content.split(":");
+    switch (l[0]) {
+      case "int":
+        return "\t${l[1]} = int(input())\n";
+        break;
+      case "float":
+        return "\t${l[1]} = float(intput())\n";
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String getCodeGo() {
+    String code = "package main\n"
+        'import "fmt"\n'
+        "func main(){\n";
+    for (_Shape s in _shapes) {
+      code += getGoShape(s);
+    }
+
+    return code + "\n}";
+  }
+
+  String getGoShape(_Shape s) {
+    switch (s.type) {
+      case "rectangulo":
+        var l = s.content.split(" ");
+        String str = '\tvar ${l[1]} ${l[0]};\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo1":
+        String str = inGo(s.content);
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo2":
+        String str = '\tfmt.Println("${s.content}");\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "rombo":
+        ifelse = 1;
+        return '\tif ${s.content} {\n\t';
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String inGo(String content) {
+    var l = content.split(":");
+    String format;
+    switch (l[0]) {
+      case "int":
+        format = "%d";
+        break;
+      case "float":
+        format = "%f";
+        break;
+    }
+    return '\tfmt.Scanf("$format",&${l[1]});\n';
+  }
+
+  String getCodeDart() {
+    String code = "import 'dart:io';\n"
+        "main(){\n";
+    for (_Shape s in _shapes) {
+      code += getDartShape(s);
+    }
+
+    return code + "\n}";
+  }
+
+  String getDartShape(_Shape s) {
+    switch (s.type) {
+      case "rectangulo":
+        var l = s.content.split(" ");
+        String str = '\tvar ${l[1]};\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo1":
+        String str = inDart(s.content);
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "paralelogramo2":
+        String str = '\tprint("${s.content}");\n';
+        if (ifelse == 1) {
+          str += "\t}";
+          ifelse = 2;
+        } else if (ifelse == 2) {
+          str = "else{\n\t\t" + str + "\t}";
+          ifelse = 0;
+        }
+        return str;
+        break;
+      case "rombo":
+        ifelse = 1;
+        return '\tif(${s.content}){\n\t';
+        break;
+      default:
+        return "";
+    }
+  }
+
+  String inDart(String content) {
+    var l = content.split(":");
+    String format;
+    switch (l[0]) {
+      case "int":
+        format = "int.parse";
+        break;
+      case "float":
+        format = "float.parse";
+        break;
+    }
+    return '\t${l[1]} = $format(stdin.readLineSync());\n';
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,342 @@
+import 'package:flutter/material.dart';
+import "package:filesystem_picker/filesystem_picker.dart";
+import 'dart:io';
+import 'package:code_maker/coder.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MainWindow());
+}
+
+class MainWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: _MainWindow_(),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class _MainWindow_ extends StatefulWidget {
+  @override
+  createState() => _MainWindow();
+}
+
+class _MainWindow extends State<_MainWindow_> {
+  String _filePath = "";
+  File file;
+  var selectedLangs = <bool>[];
+  var checkboxes;
+  bool generated = false;
+  var _textControllers = [];
+  var langs = <String>[
+    "C",
+    "C++",
+    "Java",
+    "Python",
+    "Go",
+    "Dart",
+  ];
+  var _scrollPreview = ScrollController();
+  Size screenSize;
+  var _previewWidth = 500.0;
+  var _cbheight = 60.0;
+  @override
+  Widget build(BuildContext context) {
+    screenSize = MediaQuery.of(context).size;
+    print(screenSize.width);
+    print(screenSize.height);
+    _previewWidth = screenSize.width - 320;
+    _cbheight = screenSize.height * 0.0857142857143;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Diagrama Coder"),
+          ),
+          body: createMainBody()),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.grey,
+        accentColor: Colors.blue,
+      ),
+    );
+  }
+
+  Widget createMainBody() {
+    checkboxes = _generateCheckBox(langs);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 300, minHeight: 700),
+          child: Container(
+            padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 200,
+                  height: 30,
+                  child: Scrollbar(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Text(
+                        "File:$_filePath",
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    RaisedButton(onPressed: _selectFile, child: Text("Open")),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    RaisedButton(
+                        onPressed: _createPreview, child: Text("Preview")),
+                  ],
+                ),
+                checkboxes[0],
+                checkboxes[1],
+                checkboxes[2],
+                checkboxes[3],
+                checkboxes[4],
+                checkboxes[5],
+                Row(
+                  children: [
+                    RaisedButton(
+                      onPressed: langSelected() ? _createFiles : null,
+                      child: Text("Create"),
+                      disabledColor: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    RaisedButton(
+                      onPressed: langSelected() ? _cleanPreview : null,
+                      child: Text("Clean"),
+                      disabledColor: Colors.blue,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        _makePreview()
+      ],
+    );
+  }
+
+  List<Widget> _generateCheckBox(List<String> langs) {
+    List<Widget> cbs = [];
+    for (int i = 0; i < langs.length; i++) {
+      if (!generated) {
+        selectedLangs.add(i == 0);
+      }
+      cbs.add(SizedBox(
+        width: 200,
+        height: _cbheight,
+        child: CheckboxListTile(
+          contentPadding: EdgeInsets.only(bottom: 5),
+          controlAffinity: ListTileControlAffinity.platform,
+          dense: true,
+          value: selectedLangs[i],
+          onChanged: (bool val) {
+            setState(() {
+              selectedLangs[i] = val;
+              print(val);
+            });
+          },
+          title: Text(langs[i]),
+        ),
+      ));
+    }
+    generated = true;
+    return cbs;
+  }
+
+  bool langSelected() {
+    var selected = false;
+    for (var i in selectedLangs) {
+      selected |= i;
+    }
+    print(selectedLangs);
+    print(selected);
+    return selected;
+  }
+
+  Widget _makePreview() {
+    return SizedBox(
+      child: Container(
+        color: Colors.grey[900],
+        child: Scrollbar(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: ListView(
+                  children: _makePreviewItems(langs),
+                  controller: _scrollPreview),
+            ),
+            isAlwaysShown: true,
+            controller: _scrollPreview),
+      ),
+      width: _previewWidth,
+      height: 500,
+    );
+  }
+
+  List<Widget> _makePreviewItems(List<String> _langs) {
+    List items = <Widget>[];
+    for (int i = 0; i < langs.length; i++) {
+      if (items.length <= langs.length * 3) {
+        _textControllers.add(TextEditingController());
+        items.add(Container(
+          color: Colors.blue,
+          width: 500,
+          height: 30,
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              langs[i],
+            ),
+          ),
+        ));
+        items.add(
+          TextField(
+            controller: _textControllers[i],
+            minLines: 5,
+            maxLines: 25,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+        );
+        items.add(Divider());
+      }
+    }
+    return items;
+  }
+
+  _selectFile() async {
+    var fileRes = await FilesystemPicker.open(
+        title: 'Open',
+        context: context,
+        rootDirectory: Directory(await getDocsPath()),
+        fsType: FilesystemType.file,
+        allowedExtensions: [".json", ".df"]);
+    file = File(fileRes);
+
+    print(file.path);
+    setState(() {
+      _filePath = file != null ? file.path : "";
+    });
+  }
+
+  Future<String> getDocsPath() async {
+    String readPath;
+    String user = Platform.environment["UserProfile"];
+    print(user);
+    if (Platform.isWindows) {
+      readPath = "$user\\Documents\\";
+    } else {
+      readPath = (await getApplicationDocumentsDirectory()).path;
+    }
+    return readPath;
+  }
+
+  _createPreview() async {
+    var data = await readJson(file);
+    var coder = Coder.fromRaw(data);
+    if (selectedLangs[0]) {
+      var cCode = coder.getCodeC();
+      _textControllers[0].text = cCode;
+    }
+
+    if (selectedLangs[1]) {
+      var cppCode = coder.getCodeCpp();
+      _textControllers[1].text = cppCode;
+    }
+
+    if (selectedLangs[2]) {
+      var javaCode = coder.getCodeJava();
+      _textControllers[2].text = javaCode;
+    }
+    if (selectedLangs[3]) {
+      var pythonCode = coder.getCodePython();
+      _textControllers[3].text = pythonCode;
+    }
+    if (selectedLangs[4]) {
+      var goCode = coder.getCodeGo();
+      _textControllers[4].text = goCode;
+    }
+    if (selectedLangs[5]) {
+      var dartCode = coder.getCodeDart();
+      _textControllers[5].text = dartCode;
+    }
+  }
+
+  _cleanPreview() {
+    for (var txt in _textControllers) {
+      txt.text = "";
+    }
+  }
+
+  _createFiles() async {
+    await _createPreview();
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path +
+        Platform.pathSeparator +
+        "CoderOutputs" +
+        Platform.pathSeparator;
+    Directory d = Directory(path);
+    if (!await d.exists()) await d.create();
+    if (selectedLangs[0]) {
+      File f = File(path + "output.c");
+      await f.create();
+      f.writeAsString(_textControllers[0].text);
+    }
+    if (selectedLangs[1]) {
+      File f = File(path + "output.cpp");
+      await f.create();
+      f.writeAsString(_textControllers[1].text);
+    }
+    if (selectedLangs[2]) {
+      File f = File(path + "output.java");
+      await f.create();
+      f.writeAsString(_textControllers[2].text);
+    }
+    if (selectedLangs[3]) {
+      File f = File(path + "output.py");
+      await f.create();
+      f.writeAsString(_textControllers[3].text);
+    }
+    if (selectedLangs[4]) {
+      File f = File(path + "output.go");
+      await f.create();
+      f.writeAsString(_textControllers[4].text);
+    }
+    if (selectedLangs[5]) {
+      File f = File(path + "output.dart");
+      await f.create();
+      f.writeAsString(_textControllers[5].text);
+    }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Done"),
+            content: Text("Files Created at $path"),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Ok"))
+            ],
+          );
+        });
+  }
+}
